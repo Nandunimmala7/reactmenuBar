40657cdae28de96166e5b1931dd14f1cf4cd0313
nk <nimmala.nandakishore@gmailcom>
2023-06-26 01:50:35 -0400

    completed until button

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Button.css'
+const STYLE = [
+    'btn--primary',
+    'btn--outline'
+]
+const SIZES = [
+    'btn--menu',
+    'btn--large'
+]
+export const button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize
+})=>{
+    const 
+}
---
+++ src/components/navbar/Menuitems.js
@@ -0,0 +1,29 @@
+
+export const Menuitems = [
+    {
+        title: "home",
+        url : "#",
+        cName : "nav-links"
+    },
+    {
+        title: "services",
+        url : "#",
+        cName : "nav-links"
+    },
+    {
+        title: "products",
+        url : "#",
+        cName : "nav-links"
+    },
+    {
+        title: "contact us",
+        url : "#",
+        cName : "nav-links"
+    },
+    {
+        title: "sign-up",
+        url : "#",
+        cName : "nav-links"
+    }
+]
+export default Menuitems
